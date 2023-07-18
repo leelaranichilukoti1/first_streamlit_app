1b0e739cdbe49dbf78f97c130700f26aa1fb6bc0
leelaranichilukoti1 <139886225+leelaranichilukoti1@users.noreply.github.com>
2023-07-18 22:20:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞 Hard-Boiled Free-Range Egg')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
