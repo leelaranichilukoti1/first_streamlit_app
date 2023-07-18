88007d82db99fb11fb27f574dc8991aa11bd9750
leelaranichilukoti1 <139886225+leelaranichilukoti1@users.noreply.github.com>
2023-07-18 20:18:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
-streamlit.title('Hello')
-streamlit.header('breakfast')
-streamlit.text('omega')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞 Hard-Boiled Free-Range Egg')
